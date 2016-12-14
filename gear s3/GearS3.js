780bb4805e825168cdf30e5a34c2be17dfeb2e07
Player <332725585@qq.com>
2016-12-14 13:41:19 +0800

    。

--- gear s3/GearS3.js
+++ gear s3/GearS3.js
@@ -24,6 +24,19 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 	}
 
 
+(lib.Symbol3 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#00FF00").s().p("AhCBDQgcgcAAgnQAAgmAcgcQAcgcAmAAQAnAAAcAcQAcAcAAAmQAAAngcAcQgcAcgnAAQgmAAgcgcg");
+	this.shape.setTransform(9.5,9.5);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.Symbol3, new cjs.Rectangle(0,0,19,19), null);
+
+
 (lib.Symbol2 = function(mode,startPosition,loop) {
 if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 
@@ -36,19 +49,76 @@ if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 }).prototype = getMCSymbolPrototype(lib.Symbol2, new cjs.Rectangle(-166.5,-166.5,333,333), null);
 
 
+(lib.Symbol1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#0000FF").s().p("AiwCxQhJhKAAhnQAAhmBJhKQBKhJBmAAQBnAABKBJQBJBKAABmQAABnhJBKQhKBJhnAAQhmAAhKhJg");
+	this.shape.setTransform(25,25);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.Symbol1, new cjs.Rectangle(0,0,50,50), null);
+
+
 // stage content:
 (lib.GearS3 = function(mode,startPosition,loop) {
 if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
 
 	// Layer 1
+	this.portMc = new lib.Symbol3();
+	this.portMc.parent = this;
+	this.portMc.setTransform(518.7,578.6,1,1,0,0,0,9.5,9.5);
+
+	this.p10 = new lib.Symbol1();
+	this.p10.parent = this;
+	this.p10.setTransform(144.1,607.9,1,1,0,0,0,25,25);
+
+	this.p9 = new lib.Symbol1();
+	this.p9.parent = this;
+	this.p9.setTransform(107.1,704.2,1,1,0,0,0,25,25);
+
+	this.p8 = new lib.Symbol1();
+	this.p8.parent = this;
+	this.p8.setTransform(107.1,810.2,1,1,0,0,0,25,25);
+
+	this.p7 = new lib.Symbol1();
+	this.p7.parent = this;
+	this.p7.setTransform(164.6,903.2,1,1,0,0,0,25,25);
+
+	this.p6 = new lib.Symbol1();
+	this.p6.parent = this;
+	this.p6.setTransform(261.1,953.2,1,1,0,0,0,25,25);
+
+	this.p5 = new lib.Symbol1();
+	this.p5.parent = this;
+	this.p5.setTransform(370.1,953.2,1,1,0,0,0,25,25);
+
+	this.p4 = new lib.Symbol1();
+	this.p4.parent = this;
+	this.p4.setTransform(459.1,890.9,1,1,0,0,0,25,25);
+
+	this.p3 = new lib.Symbol1();
+	this.p3.parent = this;
+	this.p3.setTransform(509.1,791.2,1,1,0,0,0,25,25);
+
+	this.p2 = new lib.Symbol1();
+	this.p2.parent = this;
+	this.p2.setTransform(509.1,692.2,1,1,0,0,0,25,25);
+
+	this.p1 = new lib.Symbol1();
+	this.p1.parent = this;
+	this.p1.setTransform(473.2,607.9,1,1,0,0,0,25,25);
+
 	this.btn = new lib.Symbol2();
 	this.btn.parent = this;
-	this.btn.setTransform(256.1,714.4);
+	this.btn.setTransform(306.1,749.4);
 
-	this.timeline.addTween(cjs.Tween.get(this.btn).wait(1));
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.btn},{t:this.p1},{t:this.p2},{t:this.p3},{t:this.p4},{t:this.p5},{t:this.p6},{t:this.p7},{t:this.p8},{t:this.p9},{t:this.p10},{t:this.portMc}]}).wait(1));
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(409.6,1116.9,333,333);
+p.nominalBounds = new cjs.Rectangle(402.1,1138.1,452.1,409.1);
 // library properties:
 lib.properties = {
 	width: 640,
--- gear s3/js/main.js
+++ gear s3/js/main.js
@@ -3,11 +3,10 @@
  */
 var MainJs = (function () {
     //上次鼠标抬起时的角度
-    var tempRoation = 0;
-    var minMoveDistance = 30;
+    var mosueUpRoation = 0;
     //最小移动角度
     var minMoveRoation = 30;
-    //鼠标按下时,表盘的角度
+    /**鼠标按下时,表盘的角度*/
     var mouseDownRoation = 0;
     //鼠标按下时,鼠标的角度
     var mouseDownArrRoation = 0;
@@ -15,6 +14,8 @@ var MainJs = (function () {
     var index = 0;
     //最大指针索引
     var maxIndex = 10;
+    //本次移动的角度
+    var tempRoation = 0;
 
     var onInit = function () {
         createjs.Touch.enable(stage);
@@ -32,38 +33,55 @@ var MainJs = (function () {
     function onMouseUp(evt) {
         console.log("onMouseUp");
         exportRoot.removeEventListener("pressmove", onUpdate)
-        tempRoation = exportRoot.btn.rotation;
+        mosueUpRoation = exportRoot.btn.rotation;
+        tempRoation = 0;
     }
 
     function onUpdate(evt) {
 
         exportRoot.btn.rotation = -Math.atan2(evt.localX - exportRoot.btn.x, evt.localY - exportRoot.btn.y) * 180 / Math.PI - (mouseDownArrRoation - mouseDownRoation);
+        tempRoation += Math.atan2(evt.localX - exportRoot.btn.x, evt.localY - exportRoot.btn.y) * 180 / Math.PI;
 
         var temp1 = exportRoot.btn.rotation - mouseDownRoation
         if (temp1 < 0) {
-            temp1
+            temp1 = 360 + temp1;
         }
-        if (Math.abs(temp1) > minMoveRoation) {
-            var temp = Math.floor((temp1) / minMoveRoation)
-            // console.log("temp" + temp);
-            if (index != temp) {
-                if (temp > index) {
-                    index++;
-                } else {
-                    index--;
+        var temp = Math.floor((temp1) / minMoveRoation)
+        if (index != temp) {
+            if (temp > index) {
+                if (temp > maxIndex) {
+                    index = 1;
+                    return;
                 }
-                updateDial()
+                index++;
+            } else {
+                if (temp < 1) {
+                    // console.log("\t index:" + index + "\t temp:" + temp + "\t ro:" + temp1);
+                    index = maxIndex;
+                    return;
+                }
+                index--;
             }
-            console.log("index:" + index + "\t temp:" + temp + "\t roation:" + exportRoot.btn.rotation);
+            updateDial()
         }
+        console.log("tempRoation:" + tempRoation);
+        // console.log("index:" + index + "\t temp:" + temp + "\t ro:" + temp1);
     }
 
     function updateDial() {
-        console.log("updateDial")
-        if (index >= maxIndex) {
+        console.log("updateDial:" + index);
+        if (index > maxIndex) {
             //该向后翻页了
         } else if (index < 1) {
             //该向前翻页了
+        }
+        // exportRoot.portMc.x = exportRoot["p" + index].x;
+        // exportRoot.portMc.y = exportRoot["p" + index].y;
+        if (exportRoot["p" + index]) {
+            createjs.Tween.get(exportRoot.portMc).to({
+                x: exportRoot["p" + index].x,
+                y: exportRoot["p" + index].y
+            }, 200, Ease.quintOut)
         }
     }
 
